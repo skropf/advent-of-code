9b11941e89487ab0b6400685691087b085eb5b08
Simon Kropf <simon.kropf@gmail.com>
2017-12-13 14:12:51 +0100

    comments

--- 13ms.py
+++ 13ms.py
@@ -8,12 +8,15 @@ firewall = {}
 for line in file:
     elems = line.strip().split(": ")
 
+    #define depth, range & steps/picoseconds needed for a whole iteration
     depth = int(elems[0])
     r = int(elems[1])
     steps = (r - 1) * 2
 
+    #adding configuration to firewall
     firewall[depth] = [r, steps]
 
+#setting max layer
 top_layer = max(firewall.keys())
 
 #Millisecond 13: First half
@@ -21,13 +24,17 @@ def calc_severity(picosecond_start=0):
     severity = 0
     picosecond = picosecond_start
     caught = False
+
     for i in range(top_layer + 1):
+        #empty layer -> increase picosecond -> start anew
         if i not in firewall.keys():
             picosecond += 1
             continue
 
+        #getting current position
         position = picosecond % firewall[i][1]
 
+        #check if caught & set severity
         if position == 0:
             caught = True
             severity += firewall[i][0] * i
@@ -40,6 +47,7 @@ print(calc_severity()[0])
 
 #Millisecond 13: Second half
 picosecond_start = 0
+#bruteforce till not getting caught anymore (may take a while)
 while True:
     if not calc_severity(picosecond_start)[1]: break
     picosecond_start += 1
