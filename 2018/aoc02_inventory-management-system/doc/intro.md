9bdff2c86887e74224480210e09354d9b485e28e
Simon Kropf <simon.kropf@gmail.com>
2018-12-05 00:38:09 +0100

    removed second day folder

--- 2018/aoc02_inventory-management-system/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to aoc02_inventory-management-system
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- 2018/aoc02_inventory-management-system/test/aoc02_inventory_management_system/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns aoc02-inventory-management-system.core-test
-  (:require [clojure.test :refer :all]
-            [aoc02-inventory-management-system.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
