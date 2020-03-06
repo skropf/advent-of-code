67d4aea8edacb9ca92bb4b6eef69685f9152b45d
skropf <simon.kropf@gmail.com>
2020-03-06 12:02:59 +0100

    finished day 11

--- 2019/11.py
+++ 2019/11.py
@@ -1,18 +1,18 @@
 ### Day 11 - Part 1
 
-puzzle_orig = open('09.input', 'r')
-puzzle = [int(x) for x in puzzle_orig.read().split(',')]
+puzzle_orig = open('11.input', 'r')
+program = [int(x) for x in puzzle_orig.read().split(',')]
 
 class IntcodeComputer(object):
     def __init__(self, name, memory):
         from copy import copy
         self.name = name
         self.memory = copy(memory)
-        self.memory += [0 for x in range(len(memory)*1000)]
+        self.memory += [0] * len(memory) * 100
         self.instruction_pointer = 0
         self.relative_base = 0
 
-    def run(self):
+    def run(self, signal):
         while True:
             op_code = self.memory[self.instruction_pointer]
             #addition
@@ -26,7 +26,7 @@ class IntcodeComputer(object):
                 else: parameter2 = self.memory[self.memory[self.instruction_pointer+2]]
                 
                 if op_code % 100000 > 20000: self.memory[self.relative_base+self.memory[self.instruction_pointer+3]] = parameter1 + parameter2
-                if op_code % 100000 > 10000: self.memory[self.instruction_pointer+3] = parameter1 + parameter2
+                elif op_code % 100000 > 10000: self.memory[self.instruction_pointer+3] = parameter1 + parameter2
                 else: self.memory[self.memory[self.instruction_pointer+3]] = parameter1 + parameter2
                 self.instruction_pointer += 4
             # multiplication
@@ -40,14 +40,14 @@ class IntcodeComputer(object):
                 else: parameter2 = self.memory[self.memory[self.instruction_pointer+2]]
                 
                 if op_code % 100000 > 20000: self.memory[self.relative_base+self.memory[self.instruction_pointer+3]] = parameter1 * parameter2
-                if op_code % 100000 > 10000: self.memory[self.instruction_pointer+3] = parameter1 * parameter2
+                elif op_code % 100000 > 10000: self.memory[self.instruction_pointer+3] = parameter1 * parameter2
                 else: self.memory[self.memory[self.instruction_pointer+3]] = parameter1 * parameter2
                 self.instruction_pointer += 4
             # take input and store
             elif op_code % 100 == 3:
-                if op_code % 1000 > 200: self.memory[self.relative_base+self.memory[self.instruction_pointer+1]] = int(input())
-                elif op_code % 1000 > 100: self.memory[self.instruction_pointer+1] = int(input())
-                else: self.memory[self.memory[self.instruction_pointer+1]] = int(input())
+                if op_code % 1000 > 200: self.memory[self.relative_base+self.memory[self.instruction_pointer+1]] = signal
+                elif op_code % 1000 > 100: self.memory[self.instruction_pointer+1] = signal
+                else: self.memory[self.memory[self.instruction_pointer+1]] = signal
                 
                 self.instruction_pointer += 2
             # outputs value at address
@@ -57,7 +57,7 @@ class IntcodeComputer(object):
                 else: parameter = self.memory[self.memory[self.instruction_pointer+1]]
                 
                 self.instruction_pointer += 2
-                print(parameter)
+                return parameter
             # jump if first parameter is not zero
             elif op_code % 100 == 5:
                 if op_code % 1000 > 200: parameter = self.memory[self.relative_base+self.memory[self.instruction_pointer+1]]
@@ -133,13 +133,67 @@ class IntcodeComputer(object):
                 self.instruction_pointer += 2
             # break
             elif op_code % 100 == 99:
-                #print("Halted.")
                 return
             else:
                 ### if printed, something is wrong
                 print('Unknown operation', op_code % 100, 'in', self.name + '.', 'Halted.')
                 return
 
-computer = IntcodeComputer("EHPR", puzzle)
+computer = IntcodeComputer("EHPR", program)
 
-computer.run()
+def paint(signal):
+    position = [0, 0]
+    view_direction = [0, -1] # look upwards
+    tiles_painted = {}
+    color, direction = 0, 0
+
+    while True:
+        color = computer.run(signal)
+        direction = computer.run(signal)
+
+        if color == None or direction == None: break
+
+        tiles_painted[tuple(position)] = color
+        if direction == 1:
+            if view_direction == [0, -1]: view_direction = [1, 0] # up -> right
+            elif view_direction == [1, 0]: view_direction = [0, 1] # right -> down
+            elif view_direction == [0, 1]: view_direction = [-1, 0] # down -> left
+            elif view_direction == [-1, 0]: view_direction = [0, -1] # left -> up
+        else:
+            if view_direction == [0, -1]: view_direction = [-1, 0] # up -> left
+            elif view_direction == [1, 0]: view_direction = [0, -1] # right -> up
+            elif view_direction == [0, 1]: view_direction = [1, 0] # down -> right
+            elif view_direction == [-1, 0]: view_direction = [0, 1] # left -> down
+        
+        position = [x + y for x, y in zip(position, view_direction)]
+
+        if tuple(position) in tiles_painted.keys(): signal = tiles_painted[tuple(position)]
+        else: signal = 0
+
+    return tiles_painted
+
+tiles = paint(0)
+print("Number of at least once painted panels:", len(tiles))
+
+### Day 11 - Part 2
+
+computer = IntcodeComputer("EHPR", program)
+tiles = paint(1)
+
+minmax = [0, 0]
+for key in tiles.keys():
+    if key[0] > minmax[1]: minmax[1] = key[0]
+    if key[1] > minmax[1]: minmax[1] = key[1]
+    if key[0] < minmax[0]: minmax[0] = key[0]
+    if key[1] < minmax[0]: minmax[0] = key[1]
+
+length = abs(minmax[0]) + abs(minmax[1]) + 1
+
+panels = [[0 for y in range(length)] for x in range(length)]
+
+for item in tiles.items():
+    panels[item[0][1]][item[0][0]] = item[1]
+
+for line in panels:
+    if 1 in line: print(line)
+    
