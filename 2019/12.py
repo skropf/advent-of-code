6710975c0c4576bcfaf2d42ebd3eb0e979318ab0
skropf <simon.kropf@gmail.com>
2020-03-06 10:56:46 +0100

    finished day 12

--- 2019/12.py
+++ 2019/12.py
@@ -1,7 +1,5 @@
 ### Day 12 - Part 1
 
-puzzle_orig = open('12.input', 'r')
-
 class Moon(object):
     def __init__(self, position):
         self.position = position
@@ -32,27 +30,27 @@ class Moon(object):
     def __repr__(self):
         return "pos=<x="+str(self.position[0])+", y="+str(self.position[1])+", z="+str(self.position[2])+">, vel=<x="+str(self.velocity[0])+", y="+str(self.velocity[1])+", z="+str(self.velocity[2])+">"
 
+def read_input():
+    puzzle_orig = open('12.input', 'r')
 
-def read_input(puzzle_orig):
-    puzzle_orig.seek(0)
     moons = []
     for line in puzzle_orig.readlines():
         line = line.strip()[1:-1].split(',')
-        pos = []
+        position = []
         for p in line:
-            pos.append(int(p.split('=')[1]))
+            position.append(int(p.split('=')[1]))
         
-        position = [pos[0], pos[1], pos[2]]
-
         moons.append(Moon(position))
     
+    puzzle_orig.close()
+
     for moon in moons:
         for neighbour in moons:
             if moon != neighbour: moon.neighbours.append(neighbour)
     
     return moons
 
-moons = read_input(puzzle_orig)
+moons = read_input()
 
 for i in range(1000):
     for moon in moons:
@@ -62,21 +60,31 @@ for i in range(1000):
 
 print("Total energy:", sum([moon.total_energy() for moon in moons]))
 
-
 ### Day 12 - Part 2
 
-moons = read_input(puzzle_orig)
+from math import gcd
+from itertools import combinations
 
-prev_state_found = False
-while not prev_state_found:
-    
-    # find a way to get find previous states
+# lowest common multiple
+def lcm(a, b):
+    return abs(a*b) // gcd(a, b)
 
+moons = read_input()
 
-    for moon in moons:
-        moon.calc_velocity()
-    for moon in moons:
-        moon.apply_step()
+starting_positions = [moon.position for moon in moons]
+x_found, y_found, z_found = False, False, False
+# different from the first part, the initial state already counts => i = 1
+i = 1
+while not x_found or not y_found or not z_found:
+    for moon in moons: moon.calc_velocity()
+    for moon in moons: moon.apply_step()
+    current_positions = [moon.position for moon in moons]
 
-print("Universe is repeating itself after", len([]), "steps.")
+    i += 1
+
+    if [c[0] for c in current_positions] == [s[0] for s in starting_positions] and not x_found: x_found = i
+    if [c[1] for c in current_positions] == [s[1] for s in starting_positions] and not y_found: y_found = i
+    if [c[2] for c in current_positions] == [s[2] for s in starting_positions] and not z_found: z_found = i
+
+print("Universe is repeating itself after", lcm(lcm(x_found, y_found), z_found), "steps.")
 
