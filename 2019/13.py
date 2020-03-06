ea8556d63daffc31dd37619ab24d549615076b3f
skropf <simon.kropf@gmail.com>
2020-03-06 12:51:23 +0100

    finished day 13-1

--- 2019/13.py
+++ 2019/13.py
@@ -1,6 +1,6 @@
-### Day 11 - Part 1
+### Day 13 - Part 1
 
-puzzle_orig = open('11.input', 'r')
+puzzle_orig = open('13.input', 'r')
 program = [int(x) for x in puzzle_orig.read().split(',')]
 
 class IntcodeComputer(object):
@@ -12,7 +12,7 @@ class IntcodeComputer(object):
         self.instruction_pointer = 0
         self.relative_base = 0
 
-    def run(self, signal):
+    def run(self):
         while True:
             op_code = self.memory[self.instruction_pointer]
             #addition
@@ -45,9 +45,9 @@ class IntcodeComputer(object):
                 self.instruction_pointer += 4
             # take input and store
             elif op_code % 100 == 3:
-                if op_code % 1000 > 200: self.memory[self.relative_base+self.memory[self.instruction_pointer+1]] = signal
-                elif op_code % 1000 > 100: self.memory[self.instruction_pointer+1] = signal
-                else: self.memory[self.memory[self.instruction_pointer+1]] = signal
+                if op_code % 1000 > 200: self.memory[self.relative_base+self.memory[self.instruction_pointer+1]] = int(input())
+                elif op_code % 1000 > 100: self.memory[self.instruction_pointer+1] = int(input())
+                else: self.memory[self.memory[self.instruction_pointer+1]] = int(input())
                 
                 self.instruction_pointer += 2
             # outputs value at address
@@ -139,61 +139,53 @@ class IntcodeComputer(object):
                 print('Unknown operation', op_code % 100, 'in', self.name + '.', 'Halted.')
                 return
 
-computer = IntcodeComputer("EHPR", program)
+computer = IntcodeComputer("Arcade", program)
 
-def paint(signal):
-    position = [0, 0]
-    view_direction = [0, -1] # look upwards
-    tiles_painted = {}
-    color, direction = 0, 0
+ids = { 0: 'empty', 1: 'wall', 2: 'block', 3: 'horizontal paddle', 4: 'ball'}
+tiles = {}
 
-    while True:
-        color = computer.run(signal)
-        direction = computer.run(None)
+while True:
+    x_pos = computer.run()
+    y_pos = computer.run()
+    tile_id = computer.run()
 
-        if color == None or direction == None: break
+    if tile_id == None: break
 
-        tiles_painted[tuple(position)] = color
-        if direction == 1:
-            if view_direction == [0, -1]: view_direction = [1, 0] # up -> right
-            elif view_direction == [1, 0]: view_direction = [0, 1] # right -> down
-            elif view_direction == [0, 1]: view_direction = [-1, 0] # down -> left
-            elif view_direction == [-1, 0]: view_direction = [0, -1] # left -> up
-        else:
-            if view_direction == [0, -1]: view_direction = [-1, 0] # up -> left
-            elif view_direction == [1, 0]: view_direction = [0, -1] # right -> up
-            elif view_direction == [0, 1]: view_direction = [1, 0] # down -> right
-            elif view_direction == [-1, 0]: view_direction = [0, 1] # left -> down
+    tiles[(x_pos, y_pos)] = ids[tile_id]
+
+print("Number of block tiles:", len([tile for tile in tiles.values() if tile == 'block']))
+
+### Day 13 - Part 2
+
+program[0] = 2
+computer = IntcodeComputer("Arcade", program)
+
+def draw_screen(tiles):
+    size = max([tile[0] for tile in tiles.keys()] + [tile[1] for tile in tiles.keys()]) + 1
+    screen = [[' ' for x in range(size)] for y in range(size)]
+
+    for tile in tiles.items():
+        screen[tile[0][0]][tile[0][1]] = tile[1]
+
+    for line in screen:
+        print(''.join(line))
+
+ids = { 0: ' ', 1: 'W', 2: 'B', 3: '|', 4: 'O'}
+tiles = {}
+current_score = 0
+while True:
+    x_pos = computer.run()
+    y_pos = computer.run()
+    tile_id = computer.run()
+
+    if tile_id == None: break
+    if x_pos == -1 and y_pos == 0:
+        current_score = tile_id
+    else:
+        tiles[(x_pos, y_pos)] = ids[tile_id]
         
-        position = [x + y for x, y in zip(position, view_direction)]
+    print("HIGHSCORE:",current_score)
+    
 
-        if tuple(position) in tiles_painted.keys(): signal = tiles_painted[tuple(position)]
-        else: signal = 0
-
-    return tiles_painted
-
-tiles = paint(0)
-print("Number of at least once painted panels:", len(tiles))
-
-### Day 11 - Part 2
-
-computer = IntcodeComputer("EHPR", program)
-tiles = paint(1)
-
-minmax = [0, 0]
-for key in tiles.keys():
-    if key[0] > minmax[1]: minmax[1] = key[0]
-    if key[1] > minmax[1]: minmax[1] = key[1]
-    if key[0] < minmax[0]: minmax[0] = key[0]
-    if key[1] < minmax[0]: minmax[0] = key[1]
-
-length = abs(minmax[0]) + abs(minmax[1]) + 1
-
-panels = [[0 for y in range(length)] for x in range(length)]
-
-for item in tiles.items():
-    panels[item[0][1]][item[0][0]] = item[1]
-
-for line in panels:
-    if 1 in line: print(line)
+    draw_screen(tiles)
     
